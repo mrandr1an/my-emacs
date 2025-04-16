407f205089d4d81edf9d2de5e8e2c1f0ec1227f2
mrandr1an <krackedissad@gmail.com>
2025-04-16 13:26:51 +0300

    add citar-org-roam

--- bibliography-management/bib-notes.el
+++ bibliography-management/bib-notes.el
@@ -8,6 +8,11 @@
   :after citar embark
   :ensure t
   :config (citar-embark-mode)
-)
+  )
+
+(use-package citar-org-roam
+  :ensure t
+  :after (citar org-roam)
+  :config (citar-org-roam-mode))
 
 (provide 'bib-notes)
